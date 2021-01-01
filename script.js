c1719256c78c2bacdc25f81883149d80099730e8
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-01 13:26:40 -0600

    coding challenge 2

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ const PersonCl = class {
     const age = 2021 - this.birthYear;
     console.log(age);
   }
-  //   when you put a method outside of the sonstructor--
+  //  when you put a method outside of the sonstructor--
   // --its going to .protoype and not the object.
 
   get futureAge() {
@@ -197,6 +197,7 @@ console.log(bankAcc.latest);
 
 // ------ Object . create ------
 
+/*
 // will be a prototype so put proto in name
 const PersonProto = {
   calAge() {
@@ -222,3 +223,52 @@ jacob.calAge();
 const beth = Object.create(PersonProto);
 beth.init('Beth', 1995);
 beth.calAge();
+
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = class {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.speed} km/h`);
+  }
+
+  get mph() {
+    const mphSpeed = this.speed / 1.6;
+    console.log(`${mphSpeed} mph`);
+  }
+
+  set newSpeed(speed) {
+    this.speed = speed * 1.6;
+    console.log(`${this.speed} km/h`);
+  }
+};
+
+const car1 = new Car('Ford', 120);
+car1.accelerate();
+car1.brake();
+car1.mph;
+car1.newSpeed = 80;
