f41f6bdb445fe3d2068de22fc95a6cc0be6986f7
Josh Gulledge <joshcalebgulledge@gmail.com>
2020-12-31 18:17:51 -0600

    more prototype

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ console.log(josh instanceof Person);
 console.log(tim instanceof Person);
 // ^ returns bool
 
-// ---- Prototypes ----
+// ------  Prototypes  ------
 
 Person.prototype.calAge = function () {
   console.log(2021 - this.birthYear);
@@ -42,6 +42,21 @@ console.log(Person.prototype);
 josh.calAge();
 bethany.calAge();
 
-console.log(josh.__proto__ === Person.prototype);
+Person.prototype.species = 'Human';
+
+// console.log(josh.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(josh));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(Person.prototype.isPrototypeOf(Person));
+console.log(josh.__proto__.__proto__);
+// console.log(josh.__proto__.__proto__.__proto__);
+
+const theArr = [2, 2, 4, 5, 5, 6, 8, 8, 10];
+console.log(theArr.__proto__);
+console.log(theArr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+// ^ dont make this a habbit
+
+console.log(theArr.unique());
